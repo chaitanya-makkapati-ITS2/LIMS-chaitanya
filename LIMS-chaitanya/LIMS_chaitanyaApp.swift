6ad351603010fcc78f4b5a07ef58026bcb08acc5
chaitanya-makkapati-ITS2 <chaitanya.makkapati@its.ny.gov>
2024-12-03 14:56:55 -0500

    Initial Commit

---
+++ LIMS-chaitanya/LIMS_chaitanyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIMS_chaitanyaApp.swift
+//  LIMS-chaitanya
+//
+//  Created by Chaitanya Makkapati on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LIMS_chaitanyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LIMS-chaitanyaTests/LIMS_chaitanyaTests.swift
@@ -0,0 +1,16 @@
+//
+//  LIMS_chaitanyaTests.swift
+//  LIMS-chaitanyaTests
+//
+//  Created by Chaitanya Makkapati on 12/3/24.
+//
+
+import Testing
+
+struct LIMS_chaitanyaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
